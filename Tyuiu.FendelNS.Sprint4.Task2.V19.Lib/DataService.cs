866a21c8ed6005e769f28ee0da63c5ed2f9f2ec0
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-15 04:19:04 +0500

    Task2

---
+++ Tyuiu.FendelNS.Sprint4.Task2.V19.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.FendelNS.Sprint4.Task2.V19.Lib
+{
+    public class DataService : ISprint4Task2V19
+    {
+        public int Calculate(int[] array)
+        {
+            int sumArray = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    sumArray += array[i];
+                }
+            }
+            return sumArray;
+        }
+    }
+}
---
+++ Tyuiu.FendelNS.Sprint4.Task2.V19/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.FendelNS.Sprint4.Task2.V19.Lib;
+namespace Tyuiu.FendelNS.Sprint4.Task2.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Фендель Н. С. | СМАРТБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы. (генератор случайных чисел)                   *");
+            Console.WriteLine("* Задание: 2                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Фендель Николай Сергеевич | СМАРТБ-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 14 элементов заполненный         *");
+            Console.WriteLine("* случайными в диапазоне от 4 до 9 подсчитать сумму нечетных элементов    *");
+            Console.WriteLine("* массива.                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите количество элементов массива: ");
+            int len = Convert.ToInt32((Console.ReadLine()));
+
+            int[] numsArray = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                numsArray[i] = rnd.Next(4, 10);
+            }
+
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write(numsArray[i] + "\t");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Сумма нечётных элементов массива = {ds.Calculate(numsArray)}");
+            Console.ReadKey();
+        }
+    }
+}
