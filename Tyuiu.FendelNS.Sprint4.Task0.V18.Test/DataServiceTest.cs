e1842f78f895e1ed8f57eb555dada327902ad0f1
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-15 04:07:51 +0500

    Add project files.

---
+++ Tyuiu.FendelNS.Sprint4.Task0.V18.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.FendelNS.Sprint4.Task0.V18.Lib;
+
+namespace Tyuiu.FendelNS.Sprint4.Task0.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] ar = { 9, 8, 7, 6, 5, 7, 3, 2, 7, 3 };
+            int res = ds.GetSumOddArrEl(ar);
+            int wait = 41;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
