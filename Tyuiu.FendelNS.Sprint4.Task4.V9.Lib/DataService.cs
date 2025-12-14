8d217fbfd0a2e2bba1351d4f235625a072761b71
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-15 04:28:18 +0500

    Task4

---
+++ Tyuiu.FendelNS.Sprint4.Task4.V9.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.FendelNS.Sprint4.Task4.V9.Lib
+{
+    public class DataService : ISprint4Task4V9
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+            int maxElement = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (array[i, j] % 2 == 0)
+                    {
+                        maxElement += array[i, j];
+                    }
+                }
+            }
+            return maxElement;
+
+        }
+    }
+}
